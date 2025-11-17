13baee86b344b24da3859776667b78ce93f72920
zack <zaccariam45@gmail.com>
2025-11-17 01:36:10 +0100

    feat: result class

--- src/AuthService.Application/Users/RegisterUserCommandHandler.cs
+++ src/AuthService.Application/Users/RegisterUserCommandHandler.cs
@@ -5,16 +5,18 @@ using System.Threading.Tasks;
 using AuthService.Application.Interfaces;
 using AuthService.Application.Interfaces.Repositories;
 using AuthService.Domain.AggregateRoots;
+using AuthService.Domain.Common.Results;
 using AuthService.Domain.Interfaces;
 using AuthService.Domain.ValueObjects;
 
 namespace AuthService.Application.Users;
 
 public record RegisterUserResponse(Guid UserId, string Email) : ICommand<Guid>;
-public record RegisterUserCommand(string firstName, string lastName, string Password, string Email) : ICommand<RegisterUserResponse>;
+public record RegisterUserCommand(string firstName, string lastName, string Password, string Email) : 
+    ICommand<Result<RegisterUserResponse>>;
 
 public class RegisterUserCommandHandler :
-    ICommandHandler<RegisterUserCommand, RegisterUserResponse>
+    ICommandHandler<RegisterUserCommand, Result<RegisterUserResponse>>
 {
     private readonly IUserRepository _userRepository;
     private readonly IPasswordHasher _passwordHasher;
@@ -27,11 +29,15 @@ public class RegisterUserCommandHandler :
         _passwordHasher = passwordHasher;
     }
 
-    public async Task<RegisterUserResponse> HandleAsync(RegisterUserCommand command, CancellationToken cancellationToken)
+    public async Task<Result<RegisterUserResponse>> HandleAsync(RegisterUserCommand command, CancellationToken cancellationToken)
     {
+        if(string.IsNullOrWhiteSpace(command.Password))
+            return Result<RegisterUserResponse>.Failure(new Error("invalid_password", "Password must be at least 6 characters long."));
+
         var exists = _userRepository.GetByEmailAsync(command.Email) is null;
         if (exists)
-            throw new InvalidOperationException("User with the given email already exists.");
+            return Result<RegisterUserResponse>.Failure(new Error("user_already_exists", "A user with the given email already exists."));
+
         var salt = _passwordHasher.GenerateSalt();
         var hashedPassword = _passwordHasher.HashPassword(command.Password, salt);
 
@@ -41,6 +47,7 @@ public class RegisterUserCommandHandler :
 
         await _userRepository.AddAsync(user);
 
-        return new RegisterUserResponse(user.Id, user.Email.Value);
+        return Result<RegisterUserResponse>.Success(
+            new RegisterUserResponse(user.Id, user.Email.Value));
     }
 }
